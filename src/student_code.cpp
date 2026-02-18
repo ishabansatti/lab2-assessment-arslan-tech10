cbeb7f99715224597cc03c4454ac51710221bb93
arslan-tech10 <muhammadarslan444777@gmail.com>
2026-02-18 02:12:17 -0800

    Update student_code.cpp

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -17,7 +17,7 @@ float getFloatValue(){
 }
 
 char getCharValue() {
-char var_char='A'
+char var_char='A';
     // Declare a variable var_char of type character and set its value to A.
     return var_char;
 }
